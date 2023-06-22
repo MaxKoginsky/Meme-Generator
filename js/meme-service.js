d47a15029167ae26ab95077598ce4b20287c8b88
MaxKoginsky <135149850+MaxKoginsky@users.noreply.github.com>
2023-06-22 19:09:58 +0300

    Add files via upload

--- js/meme-service.js
+++ js/meme-service.js
@@ -1,18 +1,30 @@
-'use strict'
+var MemeService = (function() {
+  var gImgs = [
+      { id: 1, url: 'imgs/1.jpg', keywords: ['keyword1', 'keyword1'] },
+      { id: 2, url: 'imgs/2.jpg', keywords: ['keyword2', 'keyword2'] },
+      { id: 3, url: 'imgs/3.jpg', keywords: ['keyword3', 'keyword3'] },
+      { id: 4, url: 'imgs/4.jpg', keywords: ['keyword4', 'keyword4'] },
+  ];
 
-var gImgs = [
-  { id: 1, url: 'imgs/1.jpg', keywords: ['keyword1', 'keyword1'] },
-  { id: 2, url: 'imgs/2.jpg', keywords: ['keyword2', 'keyword2'] },
-  { id: 3, url: 'imgs/3.jpg', keywords: ['keyword3', 'keyword3'] },
-  { id: 4, url: 'imgs/4.jpg', keywords: ['keyword4', 'keyword4'] },
-]
+  var gMeme = {
+      selectedImgId: null,
+      selectedLineIdx: null,
+      lines: [
+          { txt: '', size: 0, color: '' }
+      ]
+  };
 
-var gMeme = {
-  selectedImgId: null,
-  selectedLineIdx: null,
-  lines: [
-    { txt: '', size: 0, color: '' }
-  ]
-}
+  var gKeywordSearchCountMap = {}
 
-var gKeywordSearchCountMap = {}
+  return {
+      getImages: function() {
+          return gImgs
+      },
+      getMeme: function() {
+          return gMeme
+      },
+      getKeywordSearchCountMap: function() {
+          return gKeywordSearchCountMap
+      }
+  }
+})()
