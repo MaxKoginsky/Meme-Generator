c294cc2fa205c277c7523e315e64da54e2b48bfb
MaxKoginsky <135149850+MaxKoginsky@users.noreply.github.com>
2023-06-24 20:51:25 +0300

    Add files via upload

--- js/meme-controllers.js
+++ js/meme-controllers.js
@@ -4,51 +4,98 @@ var MemeController = (function(MemeService) {
     var fontSize = 36
     var textColor = '#ffffff'
     var lines = []
+    var selectedFont = 'Arial'
+    var strokeColor = '#000000'
+    var selectedLineIndex = null;
+    var canvas = document.querySelector('.canvas')
+    var ctx = canvas.getContext('2d')
+
+    function changeStrokeColor() {
+        var colorPicker = document.getElementById('strokeColorPicker')
+        strokeColor = colorPicker.value
+        renderMeme(selectedImagePath)
+    }
+
+    function clearText() {
+        enteredText = ''
+        renderMeme(selectedImagePath)
+    }
+
+    function changeFont() {
+        var fontSelect = document.querySelector('.font-select')
+        selectedFont = fontSelect.value
+        renderMeme(selectedImagePath)
+    }
 
     function addText() {
         var textInput = document.querySelector('.text-input')
         enteredText = textInput.value
-        renderMeme(selectedImagePath, enteredText)
+        addLine()
         textInput.value = ''
+        enteredText = ''
     }
 
-    function renderMeme(imagePath, text) {
-        var canvas = document.querySelector('.canvas')
-        var ctx = canvas.getContext('2d')
-    
+    function addLine() {
+        var line = {
+            txt: enteredText,
+            size: fontSize,
+            color: textColor,
+            font: selectedFont,
+            x: canvas.width / 2,
+            y: lines.length * 60 + 50,
+            width: 0,
+            height: 0
+        }
+        lines.push(line)
+        MemeService.addLine(line)
+        renderMeme(selectedImagePath)
+    }
+
+    function renderMeme(imagePath) {
+
         selectedImagePath = imagePath
     
         var image = new Image()
     
         image.onload = function() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
             ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
     
-            if (text) {
-                ctx.font = fontSize + 'px Arial'
-                ctx.fillStyle = textColor
-                ctx.strokeStyle = '#000000'
+            lines.forEach(function(line) {
+                ctx.font = line.size + 'px ' + line.font
+                ctx.fillStyle = line.color
+                ctx.strokeStyle = strokeColor
                 ctx.lineWidth = 2
                 ctx.textAlign = 'center'
     
-                var textX = canvas.width / 2
-                var textY = 50
+                var textX = line.x
+                var textY = line.y
                 
-                ctx.strokeText(text, textX, textY)
-                ctx.fillText(text, textX, textY)
-            }
+                ctx.strokeText(line.txt, textX, textY)
+                ctx.fillText(line.txt, textX, textY)
+            })
+            
+            ctx.font = fontSize + 'px ' + selectedFont
+            ctx.fillStyle = textColor
+            ctx.strokeStyle = strokeColor
+            ctx.textAlign = 'center'
+            ctx.strokeText(enteredText, canvas.width / 2, lines.length * 60 + 50)
+            ctx.fillText(enteredText, canvas.width / 2, lines.length * 60 + 50)
         }
     
         image.src = imagePath
-    }
+    } 
 
     function increaseFontSize() {
         fontSize += 2
-        renderMeme(selectedImagePath, enteredText)
+        renderMeme(selectedImagePath)
     }
 
     function decreaseFontSize() {
-        fontSize -= 2
-        renderMeme(selectedImagePath, enteredText)
+        if(fontSize > 2){
+            fontSize -= 2
+        }
+        renderMeme(selectedImagePath)
     }
 
     function getText() {
@@ -58,8 +105,8 @@ var MemeController = (function(MemeService) {
     function changeTextColor() {
         var colorPicker = document.getElementById('colorPicker')
         textColor = colorPicker.value
-        renderMeme(selectedImagePath, getText())
-    }
+        renderMeme(selectedImagePath)
+    }  
 
     function showEditor(imagePath) {
         var gallerySection = document.querySelector('.gallery')
@@ -69,7 +116,49 @@ var MemeController = (function(MemeService) {
         editorSection.classList.remove('hidden')
 
         selectedImagePath = imagePath
-        renderMeme(selectedImagePath, enteredText)
+        renderMeme(selectedImagePath)
+    }
+
+    function handleTextInput() {
+        enteredText = document.querySelector('.text-input').value
+        renderMeme(selectedImagePath)
+    }
+
+    document.querySelector('.text-input').addEventListener('input', handleTextInput)
+
+    canvas.addEventListener('mousedown', function(event) {
+        var rect = canvas.getBoundingClientRect()
+        var x = event.clientX - rect.left
+        var y = event.clientY - rect.top
+        selectedLineIndex = getSelectedLineIndex(x, y)
+    });
+
+    canvas.addEventListener('mousemove', function(event) {
+        if (selectedLineIndex != null) {
+            var rect = canvas.getBoundingClientRect()
+            var x = event.clientX - rect.left
+            var y = event.clientY - rect.top
+            lines[selectedLineIndex].x = x
+            lines[selectedLineIndex].y = y
+            renderMeme(selectedImagePath)
+        }
+    })
+
+    canvas.addEventListener('mouseup', function(event) {
+        selectedLineIndex = null
+    })
+
+    function getSelectedLineIndex(x, y) {
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i]
+            var textWidth = ctx.measureText(line.txt).width
+            var textHeight = line.size
+            if (x >= line.x - textWidth / 2 && x <= line.x + textWidth / 2 &&
+                y >= line.y - textHeight && y <= line.y) {
+                return i
+            }
+        }
+        return null
     }
 
     return {
@@ -79,6 +168,10 @@ var MemeController = (function(MemeService) {
         changeTextColor: changeTextColor,
         getText: getText,
         renderMeme: renderMeme,
-        showEditor: showEditor
+        showEditor: showEditor,
+        addLine: addLine,
+        changeFont: changeFont,
+        clearText: clearText,
+        changeStrokeColor: changeStrokeColor
     }
 })(MemeService)
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,30 +1,42 @@
 var MemeService = (function() {
-  var gImgs = [
-      { id: 1, url: 'imgs/1.jpg', keywords: ['keyword1', 'keyword1'] },
-      { id: 2, url: 'imgs/2.jpg', keywords: ['keyword2', 'keyword2'] },
-      { id: 3, url: 'imgs/3.jpg', keywords: ['keyword3', 'keyword3'] },
-      { id: 4, url: 'imgs/4.jpg', keywords: ['keyword4', 'keyword4'] },
-  ];
-
-  var gMeme = {
-      selectedImgId: null,
-      selectedLineIdx: null,
-      lines: [
-          { txt: '', size: 0, color: '' }
-      ]
-  };
-
-  var gKeywordSearchCountMap = {}
-
-  return {
-      getImages: function() {
-          return gImgs
-      },
-      getMeme: function() {
-          return gMeme
-      },
-      getKeywordSearchCountMap: function() {
-          return gKeywordSearchCountMap
-      }
-  }
+    var gImgs = [
+        { id: 1, url: 'imgs/1.jpg', keywords: ['keyword1', 'keyword1'] },
+        { id: 2, url: 'imgs/2.jpg', keywords: ['keyword2', 'keyword2'] },
+        { id: 3, url: 'imgs/3.jpg', keywords: ['keyword3', 'keyword3'] },
+        { id: 4, url: 'imgs/4.jpg', keywords: ['keyword4', 'keyword4'] },
+    ];
+  
+    var gMeme = {
+        selectedImgId: null,
+        selectedLineIdx: null,
+        lines: [
+            { txt: '', size: 0, color: '', x: 0, y: 0, width: 0, height: 0 }
+        ]
+    };
+  
+    var gKeywordSearchCountMap = {}
+  
+    return {
+        getImages: function() {
+            return gImgs
+        },
+        getMeme: function() {
+            return gMeme
+        },
+        getKeywordSearchCountMap: function() {
+            return gKeywordSearchCountMap
+        },
+        addLine: function(line) {
+            gMeme.lines.push(line)
+        },
+        deleteLine: function(lineIdx) {
+            gMeme.lines.splice(lineIdx, 1)
+        },
+        selectLine: function(lineIdx) {
+            gMeme.selectedLineIdx = lineIdx
+        },
+        updateLine: function(lineIdx, line) {
+            gMeme.lines[lineIdx] = line
+        }
+    }
 })()
